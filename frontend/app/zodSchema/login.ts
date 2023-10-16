ceced89c6a85ed5989927d7c186b9cdd78e9c00c
Ryan Henrique <ryanhardflip@gmail.com>
2023-10-15 21:07:26 -0300

    feat

--- frontend/app/zodSchema/login.ts
+++ frontend/app/zodSchema/login.ts
@@ -6,3 +6,33 @@ export const loginSchema = z.object({
 });
 
 export type User = z.infer<typeof loginSchema>;
+
+// name
+// "Ryan Henrique"
+// userName
+// "Ryanzitto"
+// email
+// "Ryanzitto@gmail.com"
+// password
+// "$2b$10$AFVGAk5gY8mzgufbhjUTDej6vpXN7EtRmv9U4Xc8pYAto/p75QOqa"
+// avatar
+// "https://avatars.githubusercontent.com/u/96020507?v=4"
+// __v
+// 0
+// bio
+// "Software developer 💻 - Skater 🛹 - Gorilla soul 🦍"
+
+// name
+// "Alice Benni"
+// userName
+// "AllieBenni"
+// email
+// "AllieBenni@gmail.com"
+// password
+// "$2b$10$quqpbcIDPXKyMQw8A3vALuzlmowNBOEIdOegFR8KciGwmhWIyzZka"
+// avatar
+// "https://burst.shopifycdn.com/photos/woman-side-profile-looking-up.jpg?…"
+// __v
+// 0
+// bio
+// "Artist, Dancer and CatLover"
