92c43ef9b62b777987c27090a996f26895fdafc4
Ryan Henrique <ryanhardflip@gmail.com>
2023-10-18 23:18:35 -0300

    feat

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -57,10 +57,40 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="flex flex-col min-h-screen h-fit bg-white justify-center items-center">
+    <main className="flex flex-col min-h-screen h-fit bg-white justify-start items-center relative">
       {load === true && (
         <div className="w-[50%] h-full flex flex-col gap-4 p-2 pt-10">
-          <div className="w-full py-4 h-fit border border-slate-300 rounded-md pl-6 flex">
+          <div className="w-full py-4 h-fit border border-slate-300 rounded-md flex">
+            <div className="w-[20%] flex justify-center items-center">
+              <div className="w-16 h-16 rounded-full bg-zinc-800 flex justify-center items-center">
+                <div
+                  className="rounded-full w-[90%] h-[90%]"
+                  style={{
+                    backgroundImage: `url(${user?.avatar})`,
+                    backgroundSize: "cover",
+                  }}
+                ></div>
+              </div>
+            </div>
+
+            <div className="flex flex-col w-[80%] justify-start items-start">
+              <div className="cursor-pointer w-[90%] border border-slate-300 bg-zinc-100 h-10 rounded-md flex items-center pl-4 transition-colors hover:bg-zinc-200">
+                <span className="font-medium text-zinc-600 text-sm">
+                  Criar publicação
+                </span>
+              </div>
+            </div>
+          </div>
+          {news !== null
+            ? news.map((post) => {
+                return <Post key={Date.now() * Math.random()} post={post} />;
+              })
+            : null}
+        </div>
+      )}
+      <div className="z-20 absolute flex justify-center items-center w-full h-screen bg-zinc-800/60">
+        <div className="w-[500px] h-fit bg-white rounded-md p-4 flex justify-center items-center flex-col ">
+          <div className="w-full flex justify-center items-center">
             <div className="w-16 h-16 rounded-full bg-zinc-800 flex justify-center items-center">
               <div
                 className="rounded-full w-[90%] h-[90%]"
@@ -70,17 +100,10 @@ export default function Home() {
                 }}
               ></div>
             </div>
-            <div className="flex justify-center items-center">
-              <span className="">{user?.userName}</span>
-            </div>
           </div>
-          {news !== null
-            ? news.map((post) => {
-                return <Post key={post} post={post} />;
-              })
-            : null}
+          <div className="w-[90%] h-fit p-4"></div>
         </div>
-      )}
+      </div>
     </main>
   );
 }
--- frontend/app/perfil/[userName]/page.tsx
+++ frontend/app/perfil/[userName]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Post } from "app/components/Post";
+import Link from "next/link";
 
 interface User {
   avatar: string;
@@ -13,17 +13,110 @@ interface User {
   bio: string;
 }
 
-interface Post {
-  banner: string;
-  comments: any[];
-  id: string;
-  likes: any[];
-  text: string;
-  title: string;
-  userName: string;
-  avatar: string;
-  createdAt: string;
+interface DateFormatOptions {
+  year?: "numeric" | "2-digit";
+  month?: "numeric" | "2-digit" | "long" | "short" | "narrow";
+  day?: "numeric" | "2-digit";
+  hour?: "numeric" | "2-digit";
+  minute?: "numeric" | "2-digit";
+  second?: "numeric" | "2-digit";
 }
+
+interface Post {
+  post: {
+    banner: string;
+    comments: Array<any>;
+    id: string;
+    likes: Array<any>;
+    text: string;
+    title: string;
+    createdAt: string;
+    user: {
+      avatar: string;
+      email: string;
+      name: string;
+      userName: string;
+      id: string;
+    };
+  };
+}
+
+const Post = ({ post }: Post) => {
+  const [load, setLoad] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoad(true);
+  }, []);
+
+  const dateFormated = (date: string) => {
+    const dataOriginal = date;
+
+    const data = new Date(dataOriginal);
+
+    const options: DateFormatOptions = {
+      month: "long",
+      day: "numeric",
+    };
+    const result = data.toLocaleDateString("pt-BR", options);
+    return result;
+  };
+
+  return (
+    <div className="w-full h-fit rounded-md flex items-center flex-col hover:bg-zinc-200/30 py-6 text-zinc-800">
+      {load === true && (
+        <>
+          <div className="w-[90%] h-20 flex justify-start items-center">
+            <div className="w-16 h-16 rounded-full bg-zinc-800 flex justify-center items-center">
+              <div
+                className="rounded-full w-[90%] h-[90%]"
+                style={{
+                  backgroundImage: `url(${post.user.avatar})`,
+                  backgroundSize: "cover",
+                }}
+              ></div>
+            </div>
+
+            <div className="flex flex-col items-start h-full pt-2 pl-2">
+              <span className="text-lg font-bold hover:opacity-80">
+                <Link href={`/perfil/${post?.user.userName}`}>
+                  {post?.user.userName}
+                </Link>
+              </span>
+              <span className="text-xs">{dateFormated(post?.createdAt)}</span>
+            </div>
+          </div>
+          <div className="w-[90%] h-fit flex flex-col p-2">
+            <h2 className="text-2xl font-black">{post.title}</h2>
+            <h2 className="text-sm font-light">{post.text}</h2>
+          </div>
+          <div className="w-full h-fit flex justify-center items-center">
+            <div className="w-[90%] h-fit pt-2 flex justify-center items-center">
+              <img className="rounded-md" src={post.banner} />
+            </div>
+          </div>
+          <div className="w-[90%] flex justify-end gap-2 pr-2 py-2">
+            <button>curtir</button>
+          </div>
+          <div className="w-[90%] flex flex-col gap-2">
+            {post.comments.map((item) => {
+              return (
+                <div
+                  key={item}
+                  className="w-full pl-4 bg-zinc-200 p-2 rounded-md flex gap-2"
+                >
+                  <span className="font-bold cursor-pointer hover:opacity-80">
+                    {item.userName}:
+                  </span>
+                  <p className="font-light">{item.comment}</p>
+                </div>
+              );
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default function Perfil({ params }: { params: { userName: string } }) {
   const [userName, setUserName] = useState<string | null>(params.userName);
@@ -52,9 +145,12 @@ export default function Perfil({ params }: { params: { userName: string } }) {
       .catch((error) => console.log(error));
   }, []);
 
+  useEffect(() => {
+    console.log(posts);
+  }, [posts]);
   return (
-    <div className="flex flex-col min-h-screen h-screen bg-white justify-start items-center">
-      <div className="w-[50%] h-full flex flex-col gap-4 pt-10">
+    <div className="flex flex-col min-h-screen h-fit py-8 bg-white justify-start items-center">
+      <div className="w-[50%] h-full flex flex-col justify-start gap-4">
         <div
           className="bg-blue-500 w-full h-[200px] relative flex justify-start items-end relative"
           style={{
@@ -87,7 +183,8 @@ export default function Perfil({ params }: { params: { userName: string } }) {
           </div>
         </div>
         {posts?.map((post) => {
-          return <Post post={post} key={post.id} />;
+          //@ts-ignore
+          return <Post post={post} key={Date.now() * Math.random()} />;
         })}
       </div>
     </div>
