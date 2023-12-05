48351e1b30da6d63f32553f22f70f8146613306c
Ryan Henrique <ryanhardflip@gmail.com>
2023-12-05 20:11:14 -0300

    feat

--- backend/src/controllers/pictureController.js
+++ backend/src/controllers/pictureController.js
@@ -1,4 +1,5 @@
 import Picture from "../models/Picture.js";
+import fs from "fs";
 
 console.log("chegou no controller");
 
@@ -14,10 +15,39 @@ const create = async (req, res) => {
     });
 
     await picture.save();
-    res.status(200).send({ message: "sucesso" });
+    res.status(200).send({ message: "sucesso", picture });
   } catch (error) {
     res.status(500).send({ message: error });
   }
 };
 
-export { create };
+const findAll = async (req, res) => {
+  try {
+    const pictures = await Picture.find();
+    res.status(200).send(pictures);
+  } catch (error) {
+    res.status(500).send({ message: "Erro ao buscar imagem." });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const picture = await Picture.findById(id);
+    console.log(id);
+    if (!picture) {
+      return res.status(404).send({ message: "Imagem não encontrada" });
+    }
+
+    console.log("aqui");
+    fs.unlinkSync(picture.src);
+    console.log("aqui");
+    await picture.remove();
+
+    res.send({ message: "Imagem removida com sucesso!" });
+  } catch (error) {
+    res.status(500).send({ message: "Erro ao buscar imagem." });
+  }
+};
+
+export { create, findAll, remove };
--- backend/src/routes/picture.route.js
+++ backend/src/routes/picture.route.js
@@ -2,10 +2,14 @@ import { Router } from "express";
 
 import upload from "../../config/multer.js";
 
-import { create } from "../controllers/pictureController.js";
+import { create, findAll, remove } from "../controllers/pictureController.js";
 
 const router = Router();
 
+router.delete("/:id", remove);
+
 router.post("/", upload.single("file"), create);
+
+router.get("/", findAll);
 
 export default router;
