079f63db1338fc4d50002444ad1c653c27c53985
Ryan Henrique <ryanhardflip@gmail.com>
2023-12-23 16:06:41 -0300

    feat

--- app/zodSchema/createPost.ts
+++ app/zodSchema/createPost.ts
@@ -3,7 +3,7 @@ import z from "zod";
 export const createPostSchema = z.object({
   title: z.string().min(1),
   text: z.string().min(1),
-  file: z.instanceof(FileList),
+  file: z.instanceof(File),
 });
 
 export type POst = z.infer<typeof createPostSchema>;
--- app/zodSchema/register.ts
+++ app/zodSchema/register.ts
@@ -7,7 +7,7 @@ export const registerSchema = z
     email: z.string().email(),
     password: z.string().min(8),
     confirmPassword: z.string(),
-    file: z.instanceof(FileList),
+    file: z.instanceof(File),
   })
   .refine((data) => data.password == data.confirmPassword, {
     message: "Senhas não coincidem",
